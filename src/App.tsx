af2ac6069e2b788983f0b8ff1f3634c33318308f
aliakseikulba <kulba.av@gmail.com>
2021-10-04 12:18:27 +0300

    lesson 02 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function Star() {
-  return (
-    <div>star</div>
-  );
+function AppTitle() {
+  console.log('AppTitle rendering');
+  return <>This is App Component</>
 }
 
-function App() {
+function Rating() {
+  console.log('Rating rendering');
   return (
     <div>
       <Star />
@@ -15,6 +15,53 @@ function App() {
       <Star />
       <Star />
       <Star />
+    </div>
+  );
+}
+
+function Accordion() {
+  console.log('Accordion rendering');
+  return (
+    <div>
+      <AccordionTitle />
+     <AccordionBody />
+    </div>
+  );
+}
+
+function AccordionTitle() {
+  console.log('AccordionTitle rendering');
+  return (
+    <h3>Меню</h3>
+  );
+}
+
+function AccordionBody() {
+  console.log('AccordionBody rendering');
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
+
+function Star() {
+  console.log('Star rendering');
+  return (
+    <div>star</div>
+  );
+}
+
+function App() {
+  console.log('App rendering');
+  return (
+    <div>
+      <AppTitle />
+      <Rating />
+      <Accordion />
+      <Rating />
     </div>
   );
 }
