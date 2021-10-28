2e696bd572d61b5e2128040f5cbfb6ed667b1fec
aliakseikulba <kulba.av@gmail.com>
2021-10-28 13:16:18 +0300

    lesson 8 tasks completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Accordion from './components/Accordion/Accordion';
 import OnOff from './components/OnOff/OnOff';
 
 
-
 function App() {
   //Rating state
   const [ratingValue, setRatingValue] = useState<RatingValueType>(0);
@@ -16,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <Accordion title='Accordion with state in App' changeStatus={setCollapsed} status={collapsed}/>
+      <Accordion title="Accordion with state in App" changeStatus={setCollapsed} status={collapsed}/>
       <Rating value={ratingValue} onClick={setRatingValue}/>
       <OnOff on={on} setOn={setOn}/>
     </div>
